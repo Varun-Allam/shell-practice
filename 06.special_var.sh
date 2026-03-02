0f86fe83798d92f20f6e0dc25ab59595bd1167d0
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2026-03-02 18:29:20 +0100

    Update 06.special_var.sh

--- 06.special_var.sh
+++ 06.special_var.sh
@@ -4,5 +4,9 @@ echo "All variables passed to the script: $@"
 echo "All variables passed to the script: $*" 
 
 echo "Script Name: $0"
+echo "Who is running: $USER" 
+echo "Current directory: $PWD"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
 
 
