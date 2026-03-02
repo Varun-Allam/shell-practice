e0e9d6f0427e94ba996fb2984681b00389d4abf8
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2026-03-02 17:09:07 +0100

    some message

---
+++ 03.Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+Person1=X
+Person2=Y
+
+echo "$Person1: Hi $Person2 "
+echo "$Person2: hello $Person1"
+echo "$Person1: what are u up to $Person2" 
+echo "$Person2: hi $Person1 Nothing" 
