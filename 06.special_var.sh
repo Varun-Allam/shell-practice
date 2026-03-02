679086d7bf530a4bf725ebbbb9f68023e05979e2
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2026-03-02 18:16:01 +0100

    Update 06.special_var.sh

--- 06.special_var.sh
+++ 06.special_var.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "All variables passed to the script:$@" 
-echo "All variables passed to the script:$*" 
+echo "All variables passed to the script: $@" 
+echo "All variables passed to the script: $*" 
 
