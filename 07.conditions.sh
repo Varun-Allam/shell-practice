03c4b487b3261cae057056e65390eaa73aae04cd
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2026-03-27 21:32:09 +0100

    Update 07.conditions.sh

--- 07.conditions.sh
+++ 07.conditions.sh
@@ -2,9 +2,9 @@
 
 NUMBER=$10
 
-if [ $NUMBER -lt 10 ]; then
+if [ "$NUMBER" -lt 10 ]; then
     echo "Given num $NUMBER is less than 10" 
-elif [ $NUMBER -gt 10 ]; then
+elif [ "$NUMBER" -gt 10 ]; then
     echo "Given num $NUMBER is greater than 10" 
 else
     echo "Given num $NUMBER is Equal to 10" 
