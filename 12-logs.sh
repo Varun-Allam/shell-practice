b511e6971449ae483a6eebcd5e116f74bdac2625
Varun-Allam <145447037+Varun-Allam@users.noreply.github.com>
2026-04-01 13:42:46 +0200

    Update 12-logs.sh

--- 12-logs.sh
+++ 12-logs.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 
 
 LOGS_FOLDER="/var/log/shell-script" 
-SCRIPT_NAME=$( basename "$0" .sh )
+SCRIPT_NAME=$( echo $0 | cut -d '.' -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
 
 mkdir -p $LOGS_FOLDER 
